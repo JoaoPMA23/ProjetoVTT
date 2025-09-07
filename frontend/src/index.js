5634dc0bb8d4329f29e2038b27d7c95e8fc19cb8
João Pedro Martins de Andrade <jpmartinsandrade123@gmail.com>
2025-09-07 10:27:45 -0300

    Initial Commit

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tabletop RPG App</h1>
+      <p>Bem-vindo ao seu RPG de mesa!</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
